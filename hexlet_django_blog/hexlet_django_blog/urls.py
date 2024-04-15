a085342866de3715f4b0ddaf50115dd1cf2f7257
Artemy Ablaev <artemyablaev.gmail.com>
2024-04-15 11:14:16 +0300

    lesson 3

--- hexlet_django_blog/hexlet_django_blog/urls.py
+++ hexlet_django_blog/hexlet_django_blog/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from hexlet_django_blog import views
 
 urlpatterns = [
+    path('about/', views.about),
+    path('', views.index),
     path('admin/', admin.site.urls),
 ]
