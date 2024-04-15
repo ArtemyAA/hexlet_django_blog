eb2332fcb16e867caf95d60d4f0ba89d2f169ace
Artemy Ablaev <artemyablaev.gmail.com>
2024-04-15 12:19:55 +0300

    lesson 4

--- hexlet_django_blog/hexlet_django_blog/settings.py
+++ hexlet_django_blog/hexlet_django_blog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hexlet_django_blog'
+    'hexlet_django_blog',
+    'hexlet_django_blog.article',
 ]
 
 MIDDLEWARE = [
--- hexlet_django_blog/hexlet_django_blog/urls.py
+++ hexlet_django_blog/hexlet_django_blog/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from hexlet_django_blog import views
 
 urlpatterns = [
     path('about/', views.about),
     path('', views.index),
     path('admin/', admin.site.urls),
+    path('articles/', include('hexlet_django_blog.article.urls'))
 ]
