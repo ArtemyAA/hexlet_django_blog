03b8ce7a1f5aa6ce7b2397b79aa1bb6f0c0f7f38
Artemy Ablaev <artemyablaev.gmail.com>
2024-04-15 15:59:54 +0300

    lesson 8

--- hexlet_django_blog/hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/hexlet_django_blog/article/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from hexlet_django_blog.article import views
 
 urlpatterns = [
-    path('', views.ArticlesView.as_view()),
+    #path('', views.ArticlesView.as_view()),
+    path('<str:tags>/<int:article_id>/', views.index, name='articles'),
+    path('', views.home, name='home')
 ]
--- hexlet_django_blog/hexlet_django_blog/article/views.py
+++ hexlet_django_blog/hexlet_django_blog/article/views.py
@@ -1,4 +1,7 @@
 from hexlet_django_blog.views import View
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse
 # Create your views here.
 
 
@@ -9,3 +12,16 @@ class ArticlesView(View):
         context = super().get_context_data(**kwargs)
         context['app_name'] = 'article'
         return context
+
+
+def index(request, tags, article_id):
+    article_text = f'Это статья номер {article_id}, по тегу {tags}'
+    return render(
+        request,
+        'articles/index.html',
+        {'article_text': article_text})
+
+
+def home(request):
+    article_url = reverse('articles', kwargs={'tags': 'python', 'article_id': 42})
+    return redirect(article_url)
